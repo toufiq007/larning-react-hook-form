9ceb5596bbecfb9fa75a7903aefeb2843c627be9
toufiq007 <t.limon2342@gmail.com>
2024-03-26 12:45:55 +0600

    react mui select data from api.country api

--- src/components/youtube_tutorial/ReactSelectMui.tsx
+++ src/components/youtube_tutorial/ReactSelectMui.tsx
@@ -1,4 +1,5 @@
-import { MenuItem, Select, TextField } from "@mui/material";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Autocomplete, MenuItem, Select, TextField } from "@mui/material";
 import { useEffect, useState } from "react";
 import { useForm, Controller } from "react-hook-form";
 
@@ -7,6 +8,7 @@ type FormValues = {
   lastName: string;
   gender: string;
   country: string;
+  courses: string[];
 };
 
 const genderList = [
@@ -24,9 +26,46 @@ const genderList = [
   },
 ];
 
+const courseList = [
+  {
+    id: 1,
+    value: "JS",
+  },
+  {
+    id: 2,
+    value: "React",
+  },
+  {
+    id: 3,
+    value: "Node",
+  },
+  {
+    id: 4,
+    value: "NextJs",
+  },
+];
+
+const skills = ["js", "react", "node", "express", "nextjs", "mongodb"];
+
+const skillOptions = skills.map((skill, index) => {
+  return {
+    id: index,
+    value: skill,
+  };
+});
+console.log(skillOptions);
+type skill = {
+  id: number;
+  value: string;
+};
+
 const ReactMuiSelect = () => {
   const { register, handleSubmit, control, reset } = useForm<FormValues>();
   const [country, setCountry] = useState([]);
+  const [value, setValue] = useState<string | null>(null);
+  const [skills, setSkills] = useState<skill | null>(null);
+
+  console.log({ value });
 
   useEffect(() => {
     fetch(`https://restcountries.com/v3.1/all`)
@@ -34,7 +73,7 @@ const ReactMuiSelect = () => {
       .then((data) => setCountry(data));
   }, []);
 
-  //   console.log(country[0]?.name?.common);
+  // console.log(country[0]?.flags?.png);
 
   const onSubmit = (data: FormValues) => {
     console.log(data);
@@ -123,6 +162,13 @@ const ReactMuiSelect = () => {
                 {country.length > 1 &&
                   country.map((country, index) => (
                     <MenuItem key={index} value={country?.name?.common}>
+                      <img
+                        src={country?.flags?.png}
+                        alt="country"
+                        width={20}
+                        height={20}
+                        style={{ marginRight: "1rem" }}
+                      />
                       {country?.name?.common}
                     </MenuItem>
                   ))}
@@ -130,6 +176,32 @@ const ReactMuiSelect = () => {
             )}
           />
         </div>
+
+        {/* <div>
+          <label htmlFor="">Choose your course</label>
+          <Controller
+            control={control}
+            name="course"
+            defaultValue={[{ id: 0, value: "html" }]}
+            render={({ field: { onChange, value } }) => (
+              <Autocomplete
+                multiple
+                id="tags-standard"
+                options={courseList}
+                getOptionLabel={(option) => option.value}
+                value={value}
+                onChange={(e, newValue) => onChange(newValue)}
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    variant="standard"
+                    placeholder="Favorites"
+                  />
+                )}
+              />
+            )}
+          />
+        </div> */}
 
         <button type="submit" style={{ color: "#fff" }}>
           Submit
